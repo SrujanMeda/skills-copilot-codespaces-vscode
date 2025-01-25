4498fbd148e1334eb509ac7ed70fb7ac1339a504
SrujanMeda <56676140+SrujanMeda@users.noreply.github.com>
2025-01-25 10:38:46 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,29 @@
+// create web server
+// http://localhost:3000/comments
+// http://localhost:3000/comments/:id
+
+const express = require('express');
+const router = express.Router();
+
+// test
+router.get('/', (req, res) => {
+    res.send('GET /comments');
+});
+
+router.post('/', (req, res) => {
+    res.send('POST /comments');
+});
+
+router.get('/:id', (req, res) => {
+    res.send('GET /comments/:id');
+});
+
+router.put('/:id', (req, res) => {
+    res.send('PUT /comments/:id');
+});
+
+router.delete('/:id', (req, res) => {
+    res.send('DELETE /comments/:id');
+});
+
+module.exports
